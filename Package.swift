ecbffd1dd48e116c2466505cfa781a7d09872878
victoriaaloy <vic.aloy@gmail.com>
2024-10-07 21:52:27 -0300

    Changes

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "shared",
-            url: "https://github.com/vicaloy/acara-shared-ios/releases/download/v0.0.15/shared.xcframework.zip", // URL directa a tu archivo .zip en GitHub
-            checksum: "4fd0836a66c72a5e91fd782fed1256fcdc9f3877aa074b129443c4606f05f406"
+            url: "https://github.com/vicaloy/acara-shared-ios/releases/download/v0.0.16/shared.xcframework.zip", // URL directa a tu archivo .zip en GitHub
+            checksum: "e2e8c607990e1a49c072d0d8e6c0d5f091193380bed8192494babe0192e3b5f6"
         )
     ]
 )
